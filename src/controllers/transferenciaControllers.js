361aa8054c21c7fb6fe65f89a95ea4aec9e74c8f
WellingtonFR <wellingtondfr@gmail.com>
2020-07-22 12:46:26 -0300

    mudanca na ordem de exibicao da index transferencia

--- src/controllers/transferenciaControllers.js
+++ src/controllers/transferenciaControllers.js
@@ -7,7 +7,7 @@ module.exports = {
     try {
       const data = await connection("transferencias")
         .select("*")
-        .orderBy("created_at", "desc");
+        .orderBy("created_at", "asc");
       return res.json(data);
     } catch (err) {
       res.status(400).send("Erro ao localizar as transferências");
